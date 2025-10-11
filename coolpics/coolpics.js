6100597ff9d12efa350fbb7b158d3fb6f139db78
colebeebe <bee14003@byui.edu>
2025-10-11 18:34:44 -0400

    Finished the rest of the project

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -18,3 +18,29 @@ function handleResize() {
 
 handleResize();
 window.addEventListener("resize", handleResize);
+
+const gallery = document.querySelector('.gallery');
+const modal = document.createElement("dialog");
+
+modal.innerHTML = "<img><button class='close-viewer'>X</button>";
+document.body.appendChild(modal);
+
+const modalImage = modal.querySelector("img");
+const closeButton = modal.querySelector("button");
+
+gallery.addEventListener("click", function(event) {
+   const image = event.target.closest('img');
+   const src = image.getAttribute("src");
+   const baseSrc = src.split("-")[0];
+   const fullSrc = baseSrc + "-full.jpeg";
+   modalImage.setAttribute('src', fullSrc);
+   modal.showModal();
+});
+
+closeButton.addEventListener("click", () => modal.close());
+
+modal.addEventListener('click', (event) => {
+   if (event.target === modal) {
+      modal.close();
+   }
+});
