d25582b9ba9eae07c39af8d2ab33db03044d3a0b
outtaspacecode <bee14003@byui.edu>
2025-11-22 18:45:01 -0500

    Recipes now display dynamically

--- recipes/main.js
+++ recipes/main.js
@@ -1,15 +1,68 @@
 import recipes from './recipes.mjs'
 
-const rName = document.querySelector(".recipe-name");
-const rDescription = document.querySelector(".recipe-description");
-const rImage = document.querySelector(".recipe-image");
-const rTags = document.querySelector(".tag-container");
-const recipe = recipes.at(-1);
+function random(num) {
+    return Math.floor(Math.random() * num);
+}
 
-rName.textContent = recipe.name;
-rDescription.textContent = recipe.description;
-rImage.src = recipe.image;
-recipe.tags.forEach((tag) => {
-    const html = `<p class="recipe-type">${tag}</p>`
-    rTags.innerHTML += html;
-});
+function getRandomListEntry(list) {
+    const listLength = list.length;
+    const randomNum = random(listLength);
+    return list[randomNum];
+}
+
+function recipeTemplate(recipe) {
+    return `<figure class="recipe">
+    <img src="${recipe.image}" alt="Image of ${recipe.name}" />
+    <figcaption>
+        <ul class="recipe__tags">
+            ${tagsTemplate(recipe.tags)}
+        </ul>
+        <h2><a href="#">${recipe.name}</a></h2>
+        <p class="recipe__ratings>
+            ${ratingTemplate(recipe.rating)}
+        </p>
+        <p class="recipe__description">
+            ${recipe.description}
+        </p>
+    </figcaption>
+    </figure>`;
+}
+
+function tagsTemplate(tags) {
+    let html = ``;
+    tags.forEach(tag => {
+        html += `<li>${tag}</li>`
+    });
+    return html;
+}
+
+function ratingTemplate(rating) {
+    let html = `<span
+        class="rating"
+        role="img"
+        aria-label="Rating: ${rating} out of 5 stars"
+    >`;
+
+    for (let i = 1; i < 6; i++) {
+        if (i <= rating) {
+            html += `<span aria-hidden="true" class="icon-star">⭐</span>`;
+        }
+        else {
+            html += `<span aria-hidden="true" class="icon-star-empty">☆</span>`;
+        }
+    }
+
+    html += `</span>`;
+    return html;
+}
+
+function renderRecipes(recipeList) {
+    const container = document.querySelector(".recipe-container");
+    recipeList.forEach(recipe => container.innerHTML += recipeTemplate(recipe));
+}
+
+function init() {
+    const recipe = getRandomListEntry(recipes);
+    renderRecipes([recipe]);
+}
+init();
